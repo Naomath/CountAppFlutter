c740d885b83f914f05a21c08e866e2191ef1d92a
naomath <math1972naonao@gmail.com>
2019-01-14 22:24:27 +0900

    part 1は終わった

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
 void main() => runApp(MyApp());
 
 //ここのファイルに全部書くのかな？
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final wordPair = WordPair.random();
-
 
     //このメソッドでUIのデザインをする
 
     return MaterialApp(
-        //ここでAndroidManifestに書いてたようなこと
-        title: 'Welcome to Flutter',
+      title: "Startup Name Generator",
+      home: RandomWords(),
+    );
+  }
+}
 
-        //ここからは画面に表示されるlayoutについて
-        home: Scaffold(
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
 
-          //ここで出てくるTextとかCenterはクラスっぽい
+class RandomWordsState extends State<RandomWords> {
+  //こいつら変数
+  final _suggestions = <WordPair>[];
 
-          //childとかは変数名だけど何にするかは変えられない感じかなぁ
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
-          appBar: AppBar(
-            title: Text("Welcome to Flutter"),
-          ),
-          body: Center(
-            child: Text(wordPair.asPascalCase),
-            //このwordはランダムに出てくる
-          ),
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
 
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
 
-        ));
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
